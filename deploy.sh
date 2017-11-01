488d00874b403c1c29acb57a2b076c58c5806b27
Evan Zlotnick <evan.zlotnick@bigcommerce.com>
2017-11-01 14:34:03 -0500

    added a test to make sure the last reading was within the last ten minutes

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+AWS_ENV=$1
+npm install
+serverless deploy
--- handler.js
+++ handler.js
@@ -1,42 +1,49 @@
 const axios = require('axios');
+const moment = require('moment-timezone');
 
 module.exports.glucose = (event, context, callback) => {
     axios.get('https://www.thezlotnicks.com/api/alexa')
         .then(function (result) {
             const data = result.data; // Expect JSON of {glucose: num, trend: num, last: string}
             let trend;
-            switch (data.trend) {
-                case 1:
-                    trend = 'the dreaded double up';
-                    break;
-                case 2:
-                    trend = 'going up';
-                    break;
-                case 3:
-                    trend = 'slightly up';
-                    break;
-                case 4:
-                    trend = 'flat';
-                    break;
-                case 5:
-                    trend = 'slightly down';
-                    break;
-                case 6:
-                    trend = 'going down';
-                    break;
-                case 7:
-                    trend = 'the dreaded double down';
-                    break;
-                default:
-                    trend = 'no trend data';
-            }
-            let say = `Zoe is at ${data.glucose} and ${trend}.`;
-            if (data.glucose >=95 && data.glucose <=150 && data.trend == 4) {
-                say += 'Rock on!';
-            } else if (data.glucose >= 285) {
-                say+= 'Stop! Insulin time';
-            } else if (data.glucose <=85) {
-                say+= 'Sugar? Yes, please!';
+            let say;
+            const lastReading = moment.tz(data.last, 'America/Chicago').unix() * 1000;
+            if ((Date.now() - lastReading) > (10 * 60 * 1000)) {
+                say = 'I do not have a current reading';
+            } else {
+                switch (data.trend) {
+                    case 1:
+                        trend = 'the dreaded double up';
+                        break;
+                    case 2:
+                        trend = 'going up';
+                        break;
+                    case 3:
+                        trend = 'slightly up';
+                        break;
+                    case 4:
+                        trend = 'flat';
+                        break;
+                    case 5:
+                        trend = 'slightly down';
+                        break;
+                    case 6:
+                        trend = 'going down';
+                        break;
+                    case 7:
+                        trend = 'the dreaded double down';
+                        break;
+                    default:
+                        trend = 'no trend data';
+                }
+                say = `Zoe is at ${data.glucose} and ${trend}.`;
+                if (data.glucose >= 95 && data.glucose <= 150 && data.trend == 4) {
+                    say += 'Rock on!';
+                } else if (data.glucose >= 285) {
+                    say += 'Stop! Insulin time';
+                } else if (data.glucose <= 85) {
+                    say += 'Sugar? Yes, please!';
+                }
             }
 
             const response = {
